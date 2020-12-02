b19ac4eebc4a80b21b82cdf50c8cf37fcaa80174
Junichi Kato <j5ik2o@gmail.com>
2020-12-02 22:12:20 +0900

    Fix run-benchmark.sh

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -4,4 +4,4 @@ rm -fr target
 rm -fr benchmark/target
 
 # sbt 'benchmark/jmh:run -prof jmh.extras.JFR:dir=.. -i 5 -wi 3 -f1 -t1'
-sbt ";clean ;benchmark/jmh:compile ;benchmark/jmh:run -i 30 -wi 20 -f1 -t1 $@"
+sbt ";clean ;benchmark/jmh:compile ;benchmark/jmh:run -i 15 -wi 15 -f1 -t1 $@"
